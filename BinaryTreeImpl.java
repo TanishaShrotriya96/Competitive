cc797e211e5bd7aa0fa693b637955df65ee03be1
tshrotri <tanisha.b.shrotriya@aexp.com>
2020-10-03 17:55:24 +0530

    Trees

--- BinaryTreeImpl.java
+++ BinaryTreeImpl.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 class BinaryTree {
 
@@ -51,14 +52,14 @@ class BinaryTree {
 
   }
 
+  //https://www.geeksforgeeks.org/bfs-vs-dfs-binary-tree/?ref=lbp
 
-
-  // display
+  // display DFS Traversal -
   public void inOrder(Node temp) {
 
     if(temp.left != null) inOrder(temp.left);
     System.out.print(temp.data+" ");
-    if(temp.right != null)  inOrder(temp.right);
+    if(temp.right != null) inOrder(temp.right);
 
   }
 
@@ -66,96 +67,255 @@ class BinaryTree {
 
     System.out.print(temp.data+" ");
     if(temp.left != null) inOrder(temp.left);
-    if(temp.right != null)  inOrder(temp.right);
+    if(temp.right != null) inOrder(temp.right);
 
   }
 
   public void postOrder(Node temp) {
 
     if(temp.left != null) inOrder(temp.left);
-    if(temp.right != null)  inOrder(temp.right);
+    if(temp.right != null) inOrder(temp.right);
     System.out.print(temp.data+" ");
   }
 
-//   // Delete a node by data value
-//   public void deleteKey(int key) {
+  // display BFS Traversal
 
-//   }
+  public void bfsTraversal(Node temp) {
 
-//   // Delete a node at given position
-//   public void deleteByPosition(int pos) {
+    if(temp==null)return ;
 
+    Queue<Node> q = new LinkedList<Node>();
+    q.add(temp);
 
+    while(q.isEmpty() == false) {
 
-//   }
+      temp=q.peek();
+      q.remove();
 
-//   // Delete a Linked List
-//   public void deleteLL() {
+      if(temp.left!=null) q.add(temp.left);
+      if(temp.right!=null) q.add(temp.right);
 
+      System.out.print(temp.data+" ");
+    }
 
-//   }
+  }
 
-//   // count number of nodes iteratively
-//   public int countNodes() {
-
-
-//   }
-
-//   // count number of nodes recursively
-//   public int countNodes(Node temp){
+  // search
+  public boolean bfsSearch(int key) {
 
 
+    if(root==null) return false;
 
-//   }
+    Queue<Node> q = new LinkedList<Node>();
+    q.add(root);
+    Node temp=null;
 
-//   // search for an element
+    while(q.isEmpty() == false){
 
-//   public boolean find(int d){
+      temp=q.peek();
+      q.remove();
+
+      if(temp.data!=key){
+        if(temp.left!=null) q.add(temp.left);
+        if(temp.right!=null) q.add(temp.right);
+      }
+
+      else{
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // dfsSearch with recursion
+  public boolean dfsRecursive(int key,Node temp){
+
+    if(temp == null) return false;
+
+    if(temp.data!=key){
+
+      boolean match = false;
+      if(temp.left!=null){
+        match = dfsRecursive(key,temp.left);
+      }
+
+      if(match==false && temp.right!=null){
+          match=dfsRecursive(key,temp.right);
+      }
+      return match;
+
+    }
+
+    return true;
+  }
+
+  //dfsSearch iterative
+  public boolean dfsIterative(int key) {
+    if(root == null ) return false;
+
+    Stack<Node> s = new Stack<Node>();
+    s.push(root);
+    Node temp=null;
+
+    while(s.isEmpty() == false) {
+
+      temp = s.pop();
+
+      if(temp.data!=key){
+
+        if(temp.right!=null) s.push(temp.right);
+        if(temp.left!=null) s.push(temp.left);
+
+      }
+
+      else return true;
+
+    }
+    return false;
+  }
 
 
-//   }
+  //insertion based on user choice
 
-//   // search for an element recursive
-//   public boolean find(int d, Node temp) {
+  public void insertAt(int parentData, String child,int nodeData) {
 
+    if(root == null) {
+      System.out.println("Tree is empty data added at root");
+      root = new Node(nodeData);
+      return;
+    }
 
-//   }
+    Node temp=returnNodeAt(parentData);
+    if(temp!=null){
 
-//   // get the Nth node of a linked list iterative approach
-//   public int getNodeAt(int pos) {
+      if(child == "left") {
+        if(temp.left == null ){
+          temp.left = new Node(nodeData);
+          return;
+        }
+        temp.left.data = nodeData;
+        return;
+      }
 
+     if(child == "right") {
 
-//   }
+        if(temp.right== null ){
+          temp.right = new Node(nodeData);
+          return;
+        }
+        temp.right.data = nodeData;
+        return;
+      }
+    }
 
-//   // get the Nth node of a linked list recursive approach
-//   public int getNodeAt(int pos,Node temp){
+    System.out.print(" Node with data "+ parentData+" was not found");
 
+  }
 
-//   }
+  public Node returnNodeAt(int key){
 
-//   // get the Nth node from the end of a linked list
-//   public int getEndNodeAt(int pos) {
+    if(root == null ) return null;
 
-//   }
+    Stack<Node> s = new Stack<Node>();
+    s.push(root);
+    Node temp=null;
+
+    while(s.isEmpty() == false) {
+
+      temp = s.pop();
+
+      if(temp.data!=key){
+
+        if(temp.right!=null) s.push(temp.right);
+        if(temp.left!=null) s.push(temp.left);
+
+      }
+
+      else return temp;
+
+    }
+    return null;
+  }
+
+  // delete by shrinking bottommost right node.
+  public boolean deleteNode(int data) {
+
+    if(root == null) return false;
+
+    Node toDelete = returnNodeAt(data);
+
+    Node temp = root;
+
+  }
 }
 
-class BinaryTreeImpl {
+class Solution {
 
   public static void main(String[] args) {
 
     BinaryTree t1 = new BinaryTree();
 
-    for(int i=0;i<12;i++) {
+    for(int i=0;i<10;i++) {
      t1.insert(t1.root,i);
     }
 
-
+    // display
     t1.inOrder(t1.root);
     System.out.println();
     t1.preOrder(t1.root);
     System.out.println();
     t1.postOrder(t1.root);
+    System.out.println();
+    t1.bfsTraversal(t1.root);
 
+    System.out.print("\nBFS Search: ");
+    System.out.print(t1.bfsSearch(1)+" ");
+    System.out.print(t1.bfsSearch(12)+" ");
+    System.out.print(t1.bfsSearch(4)+" ");
+    System.out.println(t1.bfsSearch(-1));
+
+    System.out.print("DFS Recursive: ");
+    System.out.print(t1.dfsRecursive(1,t1.root)+" ");
+    System.out.print(t1.dfsRecursive(4,t1.root)+" ");
+    System.out.print(t1.dfsRecursive(12,t1.root)+" ");
+    System.out.println(t1.dfsRecursive(-1,t1.root)+" ");
+
+    System.out.print("DFS Iterative: ");
+    System.out.print(t1.dfsIterative(1)+" ");
+    System.out.print(t1.dfsIterative(4)+" ");
+    System.out.print(t1.dfsIterative(12)+" ");
+    System.out.println(t1.dfsIterative(-1)+" ");
+
+    t1.insertAt(5,"right",15);
+    //below is a testcase
+    t1.insertAt(5,"right",15);
+    t1.bfsTraversal(t1.root);
+    System.out.println();
+
+    t1.insertAt(5,"right",10);
+    t1.bfsTraversal(t1.root);
+    System.out.println();
+    t1.insertAt(10,"right",11);
+    t1.bfsTraversal(t1.root);
+    System.out.println();
+    t1.insertAt(11,"right",12);
+    t1.bfsTraversal(t1.root);
+    System.out.println();
+    t1.insertAt(12,"left",15);
+    t1.bfsTraversal(t1.root);
+    System.out.println();
+
+    t1.insertAt(9,"left",13);
+    t1.bfsTraversal(t1.root);
+    System.out.println();
+    t1.insertAt(13,"left",14);
+    t1.bfsTraversal(t1.root);
+    System.out.println();
+
+    t1.insertAt(6,"left",16);
+    t1.bfsTraversal(t1.root);
+    System.out.println();
 
   }
 
